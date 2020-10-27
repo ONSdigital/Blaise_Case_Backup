9f8fb7e218ac98a0b691064c9e80d9effc3e7e0b
Nik Webb <71639344+nikwebbons@users.noreply.github.com>
2020-10-27 16:13:42 +0000

    changed to EnviromentVariables

---
+++ BlaiseCaseBackup/Extensions/ConfigurationExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaiseCaseBackup.Extensions
+{
+    public static class ConfigurationExtensionMethods
+    {
+        public static string ThrowExceptionIfNotNull(this string enviromentVariable)
+        {
+            throw new ConfigurationErrorsException($"No value found for enviroment variable '{enviromentVariable}'");
+        }
+    }
+}
--- BlaiseCaseBackup/Providers/ConfigurationProvider.cs
+++ BlaiseCaseBackup/Providers/ConfigurationProvider.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Configuration;
+using BlaiseCaseBackup.Extensions;
 using BlaiseCaseBackup.Interfaces;
 
 namespace BlaiseCaseBackup.Providers
 {
     public class ConfigurationProvider : IConfigurationProvider
     {
-        public string BucketName => ConfigurationManager.AppSettings["BucketName"];
+     
+        public string BucketName => Environment.GetEnvironmentVariable("ENV_BCB_BUCKET_NAME", EnvironmentVariableTarget.Machine)
+                                    .ThrowExceptionIfNotNull();
 
         public string VmName => Environment.MachineName;
 
         public string ProjectId => Environment.GetEnvironmentVariable("ENV_PROJECT_ID", EnvironmentVariableTarget.Machine)
-                                   ?? ConfigurationManager.AppSettings["ProjectId"];
-
-        public string SubscriptionTopicId => ConfigurationManager.AppSettings["SubscriptionTopicId"];
-
-        public string SubscriptionId => ConfigurationManager.AppSettings["SubscriptionId"];
-
-        public string DeadletterTopicId => ConfigurationManager.AppSettings["DeadletterTopicId"];
-
-        public string LocalBackupFolder => ConfigurationManager.AppSettings["LocalBackupFolder"];
+                                    .ThrowExceptionIfNotNull();
+        public string SubscriptionTopicId => Environment.GetEnvironmentVariable("ENV_BCB_SUB_TOPIC", EnvironmentVariableTarget.Machine)
+                                    .ThrowExceptionIfNotNull();
+        public string SubscriptionId => Environment.GetEnvironmentVariable("ENV_BCB_SUB_SUBS", EnvironmentVariableTarget.Machine)
+                                    .ThrowExceptionIfNotNull();
+        public string DeadletterTopicId => Environment.GetEnvironmentVariable("ENV_DEADLETTER_TOPIC", EnvironmentVariableTarget.Machine)
+                                    .ThrowExceptionIfNotNull();
+        public string LocalBackupFolder => Environment.GetEnvironmentVariable("ENV_BCB_LOCAL_BACKUP_DIR", EnvironmentVariableTarget.Machine)
+                                    .ThrowExceptionIfNotNull();
         public string SettingsFolder => Environment.GetEnvironmentVariable("ENV_SETTINGS_DIRECTORY", EnvironmentVariableTarget.Machine)
-                                        ?? ConfigurationManager.AppSettings["SettingsFolder"];
+                                    .ThrowExceptionIfNotNull();
     }
 }
